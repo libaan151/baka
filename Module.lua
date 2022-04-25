5391a498b1223c803a409f13067c5ceba239285e
libaan151 <97074714+libaan151@users.noreply.github.com>
2022-04-25 13:18:35 +0200

    Update Module.lua

--- Module.lua
+++ Module.lua
@@ -2,7 +2,7 @@ if getgenv().Aiming then return getgenv().Aiming end
 
 -- // Dependencies
 local SignalManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/libaan151/Signal/main/Manager.lua"))()
-local BeizerManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/libaan151/Aiming/main/BeizerManager.lua"))()
+local BeizerManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/libaan151/bruh-/main/bruhh"))()
 
 -- // Services
 local Players = game:GetService("Players")
